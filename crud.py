206bd4291f46696166b21a529902b54857a73be0
Artem <artemgul4@gmail.com>
2023-06-12 19:43:55 +0300

    args added

--- crud.py
+++ crud.py
@@ -4,7 +4,8 @@ conn = sqlite3.connect('db.db')
 
 sql = conn.cursor()
 
-def create(x):
-    print(x)
-    sql.execute("insert into persons values(?, ?, ?, ?)", x)
+
+def add_person(*args):
+    print(args)
+    sql.execute("insert into persons values(?, ?, ?, ?)", args)
     conn.commit()
